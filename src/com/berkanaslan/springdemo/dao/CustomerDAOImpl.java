6962da8efb016478267f9306f679da14fd6065b0
Berkan Aslan <berkan.aslan@patikaglobal.com>
2021-02-25 20:40:05 +0300

    DAO impl ok

---
+++ src/com/berkanaslan/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,15 @@
+package com.berkanaslan.springdemo.dao;
+
+import java.util.List;
+
+import com.berkanaslan.springdemo.entity.Customer;
+
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Override
+	public List<Customer> getCustomer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
