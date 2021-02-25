942e2b299f775f5afdd27d512d0d64cb8d4483a6
Berkan Aslan <berkan.aslan@patikaglobal.com>
2021-02-25 22:27:43 +0300

    getCustomers ok.

--- src/com/berkanaslan/springdemo/dao/CustomerDAOImpl.java
+++ src/com/berkanaslan/springdemo/dao/CustomerDAOImpl.java
@@ -2,14 +2,39 @@ package com.berkanaslan.springdemo.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.berkanaslan.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
+	// Need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+
 	@Override
-	public List<Customer> getCustomer() {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// Get the current Hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// Create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		// Execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		// Return the results
+		return customers;
+		
 	}
 
 }
